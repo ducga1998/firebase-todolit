05562a3e6903770708cac88f5fd825a775147123
Nguyễn Minh Đức <duc2820@gmail.com>
2019-08-10 12:58:33 +0700

    init project and config firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
 
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCe4cMlbPBEXnslEZImJmXipWbnk2mfBV4",
+  authDomain: "todolit.firebaseapp.com",
+  databaseURL: "https://todolit.firebaseio.com",
+  projectId: "todolit",
+  storageBucket: "todolit.appspot.com",
+  messagingSenderId: "606802076189",
+  appId: "1:606802076189:web:c21820ced69e4156"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 function App() {
   return (
     <div className="App">
