ace550f2870fa1744e7d2ed68d189c95fe37c379
Nguyễn Minh Đức <duc2820@gmail.com>
2019-08-10 13:31:54 +0700

    connect into database realtime firebase

--- src/App.js
+++ src/App.js
@@ -7,39 +7,42 @@ import * as firebase from "firebase/app";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
-
+import "firebase/database"
 // TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCe4cMlbPBEXnslEZImJmXipWbnk2mfBV4",
-  authDomain: "todolit.firebaseapp.com",
-  databaseURL: "https://todolit.firebaseio.com",
-  projectId: "todolit",
-  storageBucket: "todolit.appspot.com",
-  messagingSenderId: "606802076189",
-  appId: "1:606802076189:web:c21820ced69e4156"
-};
+ (async () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCe4cMlbPBEXnslEZImJmXipWbnk2mfBV4",
+    authDomain: "todolit.firebaseapp.com",
+    databaseURL: "https://todolit.firebaseio.com",
+    projectId: "todolit",
+    storageBucket: "todolit.appspot.com",
+    messagingSenderId: "606802076189",
+    appId: "1:606802076189:web:c21820ced69e4156"
+  };
+  
+  // Initialize Firebase'
+  console.log('bo may run ')
+  await firebase.initializeApp(firebaseConfig);
+})()
+class App extends React.Component {
+ getDataToFireBase  = async () => {
+   console.log('firebase',firebase)
+   const database = firebase.database()
+   console.log('databae',database)
+  // let userId = firebase.auth().currentUser.uid;
+  const data = await firebase.database().ref('/todolit/')
+  console.log('data',data)
+ }
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+   this.getDataToFireBase()
+  
+    return (
+      <div>
+        sacasc
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/container.js
@@ -0,0 +1,8 @@
+import Container from 'unstated-x'
+class TodoContainer extends Container {
+    todo = []
+    constructor(){
+        
+    }
+}
+const todoContainer =  new TodoContainer()
